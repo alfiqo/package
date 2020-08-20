41f2383bfa5d1c18687d2c79cce8f2db87444c87
Alfiqo Jundullah <phicko93@gmail.com>
2020-08-20 16:19:52 +0700

    show list packages

--- database/migrations/2020_08_20_075910_create_packages_table.php
+++ database/migrations/2020_08_20_075910_create_packages_table.php
@@ -14,6 +14,7 @@ class CreatePackagesTable extends Migration
     public function up()
     {
         Schema::create('packages', function ($collection) {
+            $collection->index('transaction_id');
             $collection->uniqe('transaction_id');
         });
     }
